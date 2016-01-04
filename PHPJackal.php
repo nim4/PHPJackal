194d4aa28c38721ccef5a2021c4f42130adab41f
nim4 <ghotbi.nima@gmail.com>
2016-01-04 18:29:19 +0200

    Update PHPJackal.php

--- PHPJackal.php
+++ PHPJackal.php
@@ -71,7 +71,7 @@ $Ignore_Abort = false;
 
 
 #---------------BOF-------------#
-error_reporting ( E_ALL );
+error_reporting (0);
 @ob_clean ();
 ignore_user_abort ( $Ignore_Abort );
 if (! isset ( $_SERVER ))
